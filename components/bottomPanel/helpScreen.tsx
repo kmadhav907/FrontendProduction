e7c611482ab102398103b29e5003b985c352087e
Rahul Sharma <sharmarahul1729@gmail.com>
2022-02-16 16:16:59 +0530

    Added Bottom Panel

--- components/bottomPanel/helpScreen.tsx
+++ components/bottomPanel/helpScreen.tsx
@@ -11,38 +11,5 @@ class helpScreen extends React.Component {
     }  
 }  
 
-// function HomeStack() {
-//     return (
-//         <Stack.Navigator
-//           initialRouteName="Home"
-//           screenOptions={{
-//             headerStyle: { backgroundColor: '#42f44b' },
-//             headerTintColor: '#fff',
-//             headerTitleStyle: { fontWeight: 'bold' }
-//           }}>
-//           <Stack.Screen
-//             name="Home"
-//             component={ProfileView}
-//             options={{ title: 'Home Page' }}/>
-//         </Stack.Navigator>
-//     );
-//   }
-  
-//   function SettingsStack() {
-//     return (
-//       <Stack.Navigator
-//         initialRouteName="Settings"
-//         screenOptions={{
-//           headerStyle: { backgroundColor: '#42f44b' },
-//           headerTintColor: '#fff',
-//           headerTitleStyle: { fontWeight: 'bold' }
-//         }}>
-//         <Stack.Screen
-//           name="Settings"
-//           component={Infoview}
-//           options={{ title: 'Setting Page' }}/>
-//       </Stack.Navigator>
-//     );
-//   }
 
 export default helpScreen;
