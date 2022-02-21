ae3e5f332fd8fe392ff8d094c97a97618afcc129
Rahul Sharma <sharmarahul1729@gmail.com>
2022-02-21 20:17:29 +0530

    Dashboard Navigator

--- components/bottomPanel/navigator.tsx
+++ components/bottomPanel/navigator.tsx
@@ -1,35 +1,52 @@
 import ProfileView from './profileScreen';
 import HelpView from './helpScreen';
 import Infoview from './infoScreen';
+import {View, Text} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const TabNavigator = createMaterialBottomTabNavigator(
-    {
-      Profile: {
-        screen: ProfileView,
-        navigationOptions: {
-          tabBarLabel: 'profile',
-        },
-      },
-      User: {
-        screen: Infoview,
-        navigationOptions: {
-          tabBarLabel: 'User',
-        },
-      },
-      Setting: {
-        screen: HelpView,
-        navigationOptions: {
-          tabBarLabel: 'Setting',
-        },
+  {
+    Profile: {
+      screen: ProfileView,
+      navigationOptions: {
+        tabBarLabel: 'profile',
+        tabBarIcon: ({tintColor}) => (
+          <View>
+            <Icon style={[{color: tintColor}]} size={25} name={'ios-home'} />
+          </View>
+        ),
       },
     },
-    {
-      barStyle: {backgroundColor: '#FFFFFF'},
+    User: {
+      screen: Infoview,
+      navigationOptions: {
+        tabBarLabel: 'User',
+        tabBarIcon: ({tintColor}) => (
+          <View>
+            <Icon style={[{color: tintColor}]} size={25} name={'ios-person'} />
+          </View>
+        ),
+      },
     },
-  );
-  
-  const Navigator = createAppContainer(TabNavigator);
+    Setting: {
+      screen: HelpView,
+      navigationOptions: {
+        tabBarLabel: 'Setting',
+        tabBarIcon: ({tintColor}) => (
+          <View>
+            <Icon style={[{color: tintColor}]} size={25} name={'ios-home'} />
+          </View>
+        ),
+      },
+    },
+  },
+  {
+    barStyle: {backgroundColor: '#FFFFFF'},
+  },
+);
+
+const Navigator = createAppContainer(TabNavigator);
 
 export default Navigator;
