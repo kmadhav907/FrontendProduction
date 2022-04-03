bb59f689881403098224b064b903cd6b646480b8
Rahulsharma468 <sharmarahul1729@gmail.com>
2022-04-03 19:36:03 +0530

    Notification

--- components/notification/Notifications.tsx
+++ components/notification/Notifications.tsx
@@ -7,6 +7,7 @@ import {
   Dimensions,
   ScrollView,
   Text,
+  TouchableOpacity,
 } from 'react-native';
 
 const deviceWidth = Dimensions.get('window').width;
@@ -30,60 +31,58 @@ class Notification extends Component<NotificationProps, {}> {
 
   render() {
     let notifications: any[] = [];
+    // let main_notif = this.props.notifications;
     this.props.notifications &&
       this.props.notifications.forEach((noti: any, i) => {
         const notification = (
-          // <Image
-          //   key={`image${i}`}
-          //   source={{uri: image}}
-          //   style={{width: deviceWidth - 20, margin: 'auto', height: 200}}
-          // />
-          <View
-            style={{
-              width: deviceWidth,
-              height: 150,
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: 'gold',
-            }}
-            key={i}>
-            <Text style={{width: '100%'}}>
-              This is a notification {i.toString()}
-            </Text>
+          <View key={i}>
+            {i.toString()}
+            <View style={styles.towText}>
+              <Text style={styles.towTextText}>TOWING REQUEST</Text>
+            </View>
+            <View style={styles.mainBorder}>
+              <View style={styles.imageContainer}>
+                <Image
+                  source={{
+                    uri: 'https://images.pexels.com/photos/1420440/pexels-photo-1420440.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+                  }}
+                  style={styles.imageStyle}
+                />
+              </View>
+              <View style={styles.desp}>
+                <View style={styles.custName}>
+                  <Text style={styles.custNameName}>
+                    {/* main_notif[i].notificationid */}
+                    {i.toString()}
+                  </Text>
+                </View>
+                <View style={styles.towRequest}>
+                  <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                      // onPress={this.handleSubmit}
+                      style={styles.buttonStyle1}>
+                      <Text style={styles.buttonTextStyle1}>Accept</Text>
+                    </TouchableOpacity>
+                  </View>
+                  <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                      // onPress={this.handleSubmit}
+                      style={styles.buttonStyle2}>
+                      <Text style={styles.buttonTextStyle2}>Reject</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </View>
+            </View>
           </View>
         );
         notifications.push(notification);
-
-        // const thisBar = (
-        //   <View
-        //     key={`bar${i}`}
-        //     style={[
-        //       styles.track,
-        //       {
-        //         width: this.itemWidth,
-        //         marginLeft: i === 0 ? 0 : BAR_SPACE,
-        //       },
-        //     ]}>
-        //     <Animated.View
-        //       style={[
-        //         styles.bar,
-        //         {
-        //           width: this.itemWidth,
-        //           transform: [{translateX: scrollBarVal}],
-        //         },
-        //       ]}
-        //     />
-        //   </View>
-        // );
-        // barArray.push(thisBar);
       });
 
     return (
-      <View style={styles.container}>
+      <View>
         <ScrollView
           horizontal
-          style={{height: 200}}
           showsHorizontalScrollIndicator={false}
           scrollEventThrottle={10}
           pagingEnabled
@@ -99,20 +98,160 @@ class Notification extends Component<NotificationProps, {}> {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
+  mainBorder: {
+    backgroundColor: '#f9d342',
+    width: '100%',
+    height: '20%',
+    elevation: 7,
+    borderRadius: 15,
     justifyContent: 'center',
+    flexDirection: 'row',
+    alignContent: 'center',
+    alignItems: 'center',
+    marginTop: -10,
   },
-  track: {
-    backgroundColor: '#ccc',
-    overflow: 'hidden',
-    height: 2,
+  imageContainer: {
+    marginLeft: 80,
+    height: 85,
+    width: 85,
+    borderRadius: 40,
+    marginRight: 25,
   },
-  bar: {
-    backgroundColor: '#5294d6',
-    height: 2,
+  imageStyle: {
+    height: 85,
+    width: 85,
+    borderRadius: 40,
+  },
+  custName: {
+    marginBottom: 10,
+  },
+  custNameName: {
+    fontSize: 20,
+    fontFamily: 'Metropolis',
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  desp: {
+    justifyContent: 'space-between',
+  },
+  towText: {
+    backgroundColor: '#f9d342',
+    width: '50%',
+    height: '7%',
+    borderRadius: 15,
+    borderBottomColor: 'black',
+    borderWidth: 2,
+    color: 'black',
+    marginTop: -60,
+    elevation: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 100,
+  },
+  towTextText: {
+    fontSize: 20,
+    fontFamily: 'Metropolis',
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  towRequest: {
+    width: '60%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  buttonContainer: {
+    width: '40%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+  buttonStyle1: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    fontSize: 10,
+  },
+  buttonStyle2: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+  buttonTextStyle1: {
+    color: 'white',
+    fontFamily: 'Mertropolis',
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  buttonTextStyle2: {
+    color: 'black',
+    fontFamily: 'Mertropolis',
+    fontWeight: 'bold',
+    fontSize: 15,
   },
 });
 
 export default Notification;
+
+{
+  /* <Image
+            key={`image${i}`}
+            source={{uri: image}}
+            style={{width: deviceWidth - 20, margin: 'auto', height: 200}}
+          />
+            <View
+              style={{
+                width: deviceWidth,
+                height: 150,
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: 'gold',
+              }}
+              key={i}>
+              <Text style={{width: '100%'}}>
+                This is a notification {i.toString()}
+              </Text>
+            </View>
+          ); */
+}
+
+// const thisBar = (
+//   <View
+//     key={`bar${i}`}
+//     style={[
+//       styles.track,
+//       {
+//         width: this.itemWidth,
+//         marginLeft: i === 0 ? 0 : BAR_SPACE,
+//       },
+//     ]}>
+//     <Animated.View
+//       style={[
+//         styles.bar,
+//         {
+//           width: this.itemWidth,
+//           transform: [{translateX: scrollBarVal}],
+//         },
+//       ]}
+//     />
+//   </View>
+// );
+// barArray.push(thisBar);
+
+// container: {
+//   flex: 1,
+//   alignItems: 'center',
+//   justifyContent: 'center',
+// },
+// track: {
+//   backgroundColor: '#ccc',
+//   overflow: 'hidden',
+//   height: 2,
+// },
+// bar: {
+//   backgroundColor: '#5294d6',
+//   height: 2,
+// },
