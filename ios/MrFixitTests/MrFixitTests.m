a4cbbf45cabbf0c93a5bfb04282e17ca086389ea
kmadhav907 <kmadhav907@gmail.com>
2022-04-29 14:59:42 +0530

    Final changes

--- ios/MrFixitTests/MrFixitTests.m
+++ ios/MrFixitTests/MrFixitTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AskMechanicsTests : XCTestCase
+@interface MrFixitTests : XCTestCase
 
 @end
 
-@implementation AskMechanicsTests
+@implementation MrFixitTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
